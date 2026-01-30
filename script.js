d1b10b79d2777b7f0e5645b3911b2d55d7c2afe9
vinayakpotdar4336 <vinayakpotdar4336@gmail.com>
2026-01-30 12:33:11 +0530

    script.js

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 // Optional JavaScript
 console.log("Vinayak Potdar Portfolio Loaded Successfully");
+function scrollLeft() {
+    document.getElementById("projectsContainer").scrollBy({
+        left: -300,
+        behavior: "smooth"
+    });
+}
+
+function scrollRight() {
+    document.getElementById("projectsContainer").scrollBy({
+        left: 300,
+        behavior: "smooth"
+    });
+}
+
